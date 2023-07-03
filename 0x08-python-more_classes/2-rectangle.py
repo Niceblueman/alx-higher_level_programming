cf5c9401ea373513f1ade3dea990cede09b667c5
Niceblueman <wintheiser761@gmail.com>
2023-07-03 15:33:15 +0100

    0x08. Python - More Classes and Objects Python: 🕧

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -14,14 +14,6 @@ class Rectangle:
             width (int, optional): _description_. Defaults to 0.
             height (int, optional): _description_. Defaults to 0.
         """
-        if (type(width) != int):
-            raise TypeError("width must be an integer")
-        if (width < 0):
-            raise ValueError("width must be >= 0")
-        if (type(height) != int):
-            raise TypeError("width must be an integer")
-        if (height < 0):
-            raise ValueError("width must be >= 0")
         self.__height = height
         self.__width = width
 
@@ -54,7 +46,7 @@ class Rectangle:
             TypeError: width must be an integer
             ValueError: width must be >= 0
         """
-        if (type(value) != int):
+        if (type(value) is not int):
             raise TypeError("width must be an integer")
         if (value < 0):
             raise ValueError("width must be >= 0")
@@ -71,7 +63,7 @@ class Rectangle:
             TypeError: height must be an integer
             ValueError: height must be >= 0
         """
-        if (type(value) != int):
+        if (type(value) is not int):
             raise TypeError("height must be an integer")
         if (value < 0):
             raise ValueError("height must be >= 0")
