f57253d81693ccaeaadda36974ed8044a620f6b2
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-08-17 02:41:47 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,13 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
-
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT
+    user,
+    host,
+    privilege_type
+FROM
+    mysql.user
+JOIN
+    mysql.db
+ON
+    mysql.user.user = mysql.db.user
+WHERE
+    (user = 'user_0d_1' OR user = 'user_0d_2')
+    AND host = 'localhost';
