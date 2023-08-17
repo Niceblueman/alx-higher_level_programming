4174e6048cdfd737dfdd7e1815a8f28e8c2ebf0c
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-08-17 02:39:08 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,8 @@
--- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+
+SELECT user, host FROM mysql.user;
+
+
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
