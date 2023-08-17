2a1c2cb369e38502b073e74f3e04e3a335dc917d
km8oz <5567515+KM8Oz@users.noreply.github.com>
2023-08-17 02:50:37 -0700

    0x0E. SQL - More queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,14 +1,3 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SELECT
-    user,
-    host,
-    privilege_type
-FROM
-    mysql.user
-JOIN
-    mysql.db
-ON
-    mysql.user.user = mysql.db.user
-WHERE
-    (user = 'user_0d_1' OR user = 'user_0d_2')
-    AND host = 'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
