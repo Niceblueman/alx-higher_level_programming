3fb3e8e702c20fe10ee5181760a1eb5e2c4255f4
Niceblueman <wintheiser761@gmail.com>
2023-10-03 01:40:31 +0100

    alldone

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a request to the specified URL and display only the status code of the response.
-url="$1" && curl -sI "$url" -o test7 && awk 'NR==1{printf "%s", $2}' test7
+awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
