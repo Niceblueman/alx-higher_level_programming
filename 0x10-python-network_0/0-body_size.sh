2745cea2393bab0f78576ae2342ff731c103c797
Niceblueman <wintheiser761@gmail.com>
2023-10-03 01:22:01 +0100

    alldone

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Calculate the byte size of the HTTP response header for a specified URL.
-url="$1" && header_size=$(curl -s "$url" | wc -c)
-echo "$header_size"
+url="$1" && header_size=$(curl -s "$url" | wc -c) && echo "$header_size"
