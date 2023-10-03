642292b95d689338fe93d3668506e229d532f961
Niceblueman <wintheiser761@gmail.com>
2023-10-03 02:09:07 +0100

    alldone

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
-"""solution"""
+"""Description: This Python script sends an HTTP GET request to a specified URL
+and prints information about the response content.
+
+Parameters:
+    - URL (str): The URL to send the GET request to.
+
+Usage:
+    Run this script by providing the URL as a command-line argument. For example:
+    $ python3 script.py https://alx-intranet.hbtn.io/status
+"""
 
 if __name__ == '__main__':
     import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <URL>")
+        sys.exit(1)
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as res:
+            content = res.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except urllib.error.URLError as e:
+        print(f"Error: Unable to retrieve data from the URL: {url}")
+        print(f"Reason: {e.reason}")
