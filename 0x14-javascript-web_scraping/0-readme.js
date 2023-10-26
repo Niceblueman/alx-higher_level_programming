ee013cc6f318cbbf6082873a46ecbb631735ff44
Niceblueman <wintheiser761@gmail.com>
2023-10-26 03:19:11 +0100

    process.argv[2]

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,3 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-const textToWrite = process.argv[3];
-
-fs.writeFile(filePath, textToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
+fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => { console.log(err); });
