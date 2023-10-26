efb7f88e72b8b30d9681a289b02a26261888e752
Niceblueman <wintheiser761@gmail.com>
2023-10-26 03:15:39 +0100

    readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
+const filePath = process.argv[2];
+const textToWrite = process.argv[3];
+
+fs.writeFile(filePath, textToWrite, 'utf-8', (err) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    console.error(err);
   }
 });
