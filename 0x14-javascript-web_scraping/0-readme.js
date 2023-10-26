5431f14b64c7426dd1d2080fe5d6865eb93659c0
Niceblueman <wintheiser761@gmail.com>
2023-10-26 03:23:37 +0100

    readFile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-import { readFile } from 'node:fs';
-readFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => { err != null ? console.log(err): console.log(data); });
+const { readFile } = require('fs');
+readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
