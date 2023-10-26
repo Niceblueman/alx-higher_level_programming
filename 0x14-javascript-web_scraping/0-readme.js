f4686d738a81385f566376333abb54d02277a14c
Niceblueman <wintheiser761@gmail.com>
2023-10-26 03:33:51 +0100

    #3

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', (err, data) => { if (err) console.error(err); else console.log(data+"\n"); });
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
